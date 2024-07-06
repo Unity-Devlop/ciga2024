a3f5f65d07682bf00b757244b19d9dfa573bc5ed
买一瓶汽水 <118652653+HEARTkkkkheart@users.noreply.github.com>
2024-07-06 14:16:26 +0800

    新建了一个Item，用于UI显示

---
+++ Assets/Game/Runtime/FoodItem.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Game
+{
+    public class FoodItem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public Image foodSprite;
+        public TextMeshProUGUI textNum;
+        public int myNum;
+        public FoodType foodType;
+        public Dictionary<FoodType, Sprite> foodObj = new Dictionary<FoodType, Sprite>();
+
+        public void SetFoodType(FoodType a)
+        {
+            foodType = a;
+        }
+
+        public void Init(int num)
+        {
+            switch (foodType)
+            {
+                case FoodType.Hamburger :
+                    foodSprite.sprite = foodObj[FoodType.Hamburger];
+                    myNum = num;
+                    textNum.text = myNum.ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+    
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }    
+}
+
