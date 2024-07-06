ca32a7b03fe4ea7514a674af926c49d46ec12790
买一瓶汽水 <118652653+HEARTkkkkheart@users.noreply.github.com>
2024-07-06 21:04:08 +0800

    结算面板

--- Assets/Game/Runtime/UI/SettlementPanel.cs
+++ Assets/Game/Runtime/UI/SettlementPanel.cs
@@ -1,17 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityToolkit;
+using DG.Tweening;
 
 namespace Game
 {
     public class SettlementPanel : UIPanel
     {
-        public EndingType endingType;
 
+        [SerializeField]public EndingType endingType;
+        
+        
+        [SerializeField]private Image image;
+        [SerializeField]private Button btnExit;
+        [SerializeField]private Button btnNext;
+        [SerializeField]private Button btnReSart;
         // Start is called before the first frame update
         void Start()
         {
+            //退出游戏
+            btnExit.onClick.AddListener((() =>
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+            }));
+            
+            
+            
+            btnNext.onClick.AddListener((() =>
+            {
+                //下一关
+                print($"下一关");
+            }));
+            
+            btnReSart.onClick.AddListener((() =>
+            {
+                //重新开始这一关
+                print($"重新开始这一关");
+            }));
+
+            image.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            image.transform.DOScale(new Vector3(1, 1, 1), 0.2f);
+
+            switch (endingType)
+            {
+                case EndingType.Perfect :
+                    break;
+                case EndingType.Common :
+                    break;
+                case EndingType.EatingDisorder:
+                    break;
+                case EndingType.EatintTooMuch:
+                    break;
+                case EndingType.Anorexia :
+                    break;
+                case EndingType.Death :
+                    break;
+                case EndingType.Starve :
+                    break;
+                case EndingType.Boom :
+                    break;
+                default:
+                    break;
+            }
+            
             
         }
     
