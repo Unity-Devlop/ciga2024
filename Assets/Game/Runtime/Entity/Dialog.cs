0ffa40040cfeb2ce233cb592a9db6c0b1c04130b
LAPTOP-FG35BCEI\FM <2412760825@qq.com>
2024-07-07 11:18:27 +0800

    [feature] 增加Level1的对话

--- Assets/Game/Runtime/Entity/Dialog.cs
+++ Assets/Game/Runtime/Entity/Dialog.cs
@@ -5,13 +5,24 @@ using UnityEngine.Serialization;
 
 namespace Game
 {
+    [RequireComponent(typeof(BoxCollider2D))]
     public class Dialog : MonoBehaviour
     {
         public DialogueTreeController dialogueTreeController;
 
+        private Collider2D _collider;
+        
+        private void Start()
+        {
+            _collider = GetComponent<BoxCollider2D>();
+            _collider.isTrigger = true;
+        }
+
+
         private void OnTriggerEnter2D(Collider2D other)
         {
             GameMgr.Singleton.StartDialog(dialogueTreeController);
+            _collider.enabled = false;
         }
     }
 }
