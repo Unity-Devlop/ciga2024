9478d2d71db331cbc3c226665af202f7e2e83c8e
买一瓶汽水 <118652653+HEARTkkkkheart@users.noreply.github.com>
2024-07-07 14:37:16 +0800

    1

---
+++ Assets/Game/Runtime/UI/AchievementPanel.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game
+{
+    public class AchievementPanel : MonoBehaviour
+    {
+        public List<string> liststr =new List<string>(){ "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+        public List<Image> listImg = new List<Image>();
+        
+        public void newstr()
+        {
+            for (int i = 0; i < 8; ++i)
+            {
+                PlayerPrefs.SetFloat(liststr[i], 0);
+            }
+        }
+
+        public void SetStr(int num)
+        {
+            PlayerPrefs.SetFloat(liststr[num],1);
+        }
+
+
+        private void Update()
+        {
+            
+            for (int i = 0; i < 8; ++i)
+            {
+                if (PlayerPrefs.GetInt(liststr[i]) == 0)
+                {
+                    listImg[i].color = Color.gray;
+                }
+                else
+                {
+                    listImg[i].color = Color.white;
+                }
+            }
+        }
+        
+        
+    }
+}
