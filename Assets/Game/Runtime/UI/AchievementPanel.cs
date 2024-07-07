656c4779b756f74c517e21672b2e51a3e12101d7
买一瓶汽水 <118652653+HEARTkkkkheart@users.noreply.github.com>
2024-07-07 14:59:46 +0800

    1111

--- Assets/Game/Runtime/UI/AchievementPanel.cs
+++ Assets/Game/Runtime/UI/AchievementPanel.cs
@@ -4,16 +4,25 @@ using System.Collections.Generic;
 using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityToolkit;
 
 namespace Game
 {
-    public class AchievementPanel : MonoBehaviour
+    public class AchievementPanel : UIPanel
     {
         public List<string> liststr =new List<string>(){ "0", "1", "2", "3", "4", "5", "6", "7", "8" };
         public List<Image> listImg = new List<Image>();
 
         public Button btnClose;
-        
+
+        private void Start()
+        {
+            btnClose.onClick.AddListener(() =>
+            {
+                UIRoot.Singleton.ClosePanel<AchievementPanel>();
+            });
+        }
+
         public void newstr()
         {
             for (int i = 0; i < 8; ++i)
